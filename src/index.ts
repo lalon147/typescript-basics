f9f88f027f2c82e52d2c2ceed0540932fb09bcb3
lalon147 <lalon.rahman147@gmail.com>
2023-05-04 16:30:43 +0600

    type alias of object and boolean data type

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+//type alias
+
+type CrushType={
+    name:string;
+    age:number;
+    profession:string
+}
+
+const crush1:CrushType={
+    name:"mohona",
+    age:19,
+    profession:"model"
+}
+
+type CrushMarried=boolean;
+
+const isCrushMarried:CrushMarried=true

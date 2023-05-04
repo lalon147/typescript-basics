82dc2dbdaa671ad7f34ded9792ff2ae6c70ace26
lalon147 <lalon.rahman147@gmail.com>
2023-05-04 18:16:53 +0600

    type alias of function

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,11 @@ const crush1:CrushType={
 
 type CrushMarried=boolean;
 
+type OperationType=(x:number,y:number)=>number
+
 const isCrushMarried:CrushMarried=true
+const calculate=(number1:number,number2:number,operation:OperationType)=>{
+       return operation(number1,number2)
+}
+
+console.log(calculate(10,20,(x,y)=>x-y))
